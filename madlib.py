c4cfa6f0834758b8c6c63be66e88209686626e04
sushma k <89923171+NagaSushmaSree@users.noreply.github.com>
2023-01-07 18:39:29 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,39 @@
+
+question="Today I went to the zoo. I saw a(n) \
+{ }(adjective) \
+{ }(Noun) jumping up and down in its tree. \
+He { }(verb, past tense) { }(adverb) \
+through the large tunnel that led to its { }(adjective) \
+{ }(noun). I got some peanuts and passed \
+them through the cage to a gigantic gray { }(noun) \
+towering above my head. Feeding that animal made \
+me hungry. I went to get a{ }(adjective) scoop of ice cream. It filled my stomach. Afterwards I had to \
+{ }(verb) { }(adverb) to catch our bus. \
+When I got home I { }(verb, past tense) my \
+mom for a { }(adjective) day at the zoo. "
+print(question)
+adject1=input("Enter an adjective ")
+noun1=input("enter a noun ")
+verb1=input("Enter a verb ")
+adverb=input("Enter a adverb ")
+adject2=input("Enter a adject ")
+noun2=input("Enter a noun ")
+noun3=input("enter a noun ")
+adject3=input("Enter a adjective ")
+verb2=input("Enter a verb ")
+adverb2=("Enter a adverb ")
+verb3=input("Enter a verb ")
+adject4=input("enter a adject ")
+madlibs= f"Today I went to the zoo. I saw a \
+{adject1}\
+{noun1}jumping up and down in its tree. \
+He {verb1}{adverb}(adverb) \
+through the large tunnel that led to its {adject2}\
+{noun2}. I got some peanuts and passed \
+them through the cage to a gigantic gray {noun3}\
+towering above my head. Feeding that animal made \
+me hungry. I went to get a{adject3} scoop of ice cream. It filled my stomach. Afterwards I had to \
+{verb2} {adverb2} to catch our bus. \
+When I got home I {verb3} my \
+mom for a {adject4}  day at the zoo. "
+print(madlibs)
